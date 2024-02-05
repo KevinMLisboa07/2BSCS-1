1deb3a81dff52f2142b5076b03d73035dd613c1a
KevinMLisboa07 <158980787+KevinMLisboa07@users.noreply.github.com>
2024-02-05 04:29:30 +0000

    // test

---
+++ Student.java
@@ -0,0 +1,31 @@
+class Student {
+    private String studentNo;
+    private String lastName;
+    private String firstName;
+
+    public String getStudentNo() {
+        return this.studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+   
+}
